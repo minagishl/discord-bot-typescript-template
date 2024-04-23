2159bd744aa79bc21fb189297d81d4f617ef7502
みなぎし <114616680+minagishl@users.noreply.github.com>
2024-04-24 02:13:32 +0900

    Update config.ts
    Signed-off-by: みなぎし <114616680+minagishl@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
-export const COLOR = {
+import { type ColorResolvable } from 'discord.js';
+
+type Color = Record<string, ColorResolvable>;
+
+export const COLOR: Color = {
   PRIMARY: '#ffffff',
   SECONDARY: '#000000',
   BLACK: '#1a1a1c',
